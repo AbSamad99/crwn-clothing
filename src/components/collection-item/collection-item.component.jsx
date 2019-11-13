2bc6e9eb69eda0dcc617ad2be896d1c0ca49ffae
AbSamad99 <abdussamad.mi5@gmail.com>
2019-11-13 17:30:36 +0530

    Functionality to add items to the cart updated

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import './collection-item.styles.scss';
 
-const CollectionItem=({name,imageUrl,price,id})=>(
+import CustomButton from '../custom-button/custom-button.component';
+import {AddCartItem} from '../../redux/cart/cart.actions';
+
+const CollectionItem=({item,AddCartItem})=>{
+    const{name,imageUrl,price}=item;
+    return(
     <div className='collection-item' >
         <div className='item-image' style={{backgroundImage:`url(${imageUrl})`}} />
         <div className='collection-footer' >
             <span className='name' >{name}</span>
             <span className='price' >{price}</span>
         </div>
+        <CustomButton onClick={()=>AddCartItem(item)} className='custom-button' inverted >Add to cart</CustomButton>
     </div>
-);
+)};
 
-export default CollectionItem;
+const mapDispatchToProps=dispatch=>({
+    AddCartItem:(item)=>dispatch(AddCartItem(item))
+})
+
+export default connect(null,mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import {cartActionTypes} from './cart.types';
 export const ToggleCartHidden=()=>({
     type:cartActionTypes.TOGGLE_CART_HIDDEN
 });
+
+export const AddCartItem=(item)=>({
+    type:cartActionTypes.ADD_CART_ITEM,
+    payload:item
+});
