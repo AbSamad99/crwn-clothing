6516787f0d4c66ea104141d9d5be4409d5bd69f4
AbSamad99 <abdussamad.mi5@gmail.com>
2019-11-15 17:08:52 +0530

    Adding ability to remove, increase and decrease quantity of items in checkout page

--- src/components/checkout-item/checkout-item.component.js
+++ src/components/checkout-item/checkout-item.component.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem=({cartItem:{name,price,quantity,imageUrl}})=>(
+import {ClearCartItem,AddCartItem,RemoveCartItem} from '../../redux/cart/cart.actions';
+
+const CheckoutItem=({cartItem,ClearCartItem,AddCartItem,RemoveCartItem})=>{
+    const {name,price,quantity,imageUrl}=cartItem;
+    return(
     <div className='checkout-item' >
         <div className='image-container' >
             <img alt='item' src={imageUrl} />
         </div>
         <span className='name' >{name}</span>
-        <span className='quantity' >{quantity}</span>
+        <span className='quantity' >
+            <div className='arrow' onClick={()=>RemoveCartItem(cartItem)} >&#10094;</div>
+            <span className='value' >{quantity}</span>
+            <div className='arrow' onClick={()=>AddCartItem(cartItem)} >&#10095;</div>
+        </span>
         <span className='price' >${price}</span>
-        <div className='remove-button' >&#10005;</div>
+        <div className='remove-button' onClick={()=>ClearCartItem(cartItem)}>&#10005;</div>
     </div>
-)
+)}
 
-export default CheckoutItem;
+const mapDispatchToProps=dispatch=>({
+    ClearCartItem:item=>dispatch(ClearCartItem(item)),
+    AddCartItem:item=>dispatch(AddCartItem(item)),
+    RemoveCartItem:item=>dispatch(RemoveCartItem(item))
+})
+
+export default connect(null,mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,13 @@ export const AddCartItem=(item)=>({
     type:cartActionTypes.ADD_CART_ITEM,
     payload:item
 });
+
+export const RemoveCartItem=(item)=>({
+    type:cartActionTypes.REMOVE_CART_ITEM,
+    payload:item
+});
+
+export const ClearCartItem=(item)=>({
+    type:cartActionTypes.CLEAR_CART_ITEM,
+    payload:item
+});
