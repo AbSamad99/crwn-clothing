ac04bfdcfa3a83e2f2aa46d8ed40a1d6845f1130
AbSamad99 <abdussamad.mi5@gmail.com>
2020-01-27 21:10:51 +0530

    updating gitignore files of both client and main

---
+++ client/src/global.styles.js
@@ -0,0 +1,27 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyle=createGlobalStyle`
+body{
+    font-family: 'Open Sans Condensed';
+    padding: 20px 40px;
+}
+
+a{
+    text-decoration: none;
+    color: black;
+}
+
+*{
+    box-sizing: border-box;
+}
+
+::-webkit-scrollbar {
+  -webkit-appearance: none;
+  width: 7px;
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 4px;
+  background-color: rgba(0,0,0,.5);
+  box-shadow: 0 0 1px rgba(255,255,255,.5);
+}
+`;
